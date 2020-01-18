825a4f8640ddfd64aeb778c552197988d84fe9f1
fybug <1006291762@qq.com>
2020-01-18 14:46:07 +0800

    去除多余依赖

--- PDWebpack/load/requireLib.js
+++ PDWebpack/load/requireLib.js
@@ -5,4 +5,3 @@ global.path = require('path'); // 路径库
 global.HtmlWebpackPlugin = require('html-webpack-plugin'); // Html 导入
 global.ScriptExtHtmlWebpackPlugin = require('script-ext-html-webpack-plugin'); // js 的加载方式
 global.MiniCssExtractPlugin = require('mini-css-extract-plugin'); // 单独提取 css
-global.glob = require('globby'); // 文件匹配
